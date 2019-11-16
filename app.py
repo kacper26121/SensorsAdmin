4fb73e1b7e31e124460cb6c79220496c3923a798
Kacper <kacper.mobilet3@gmail.com>
2019-11-16 09:14:46 +0100

    * Dodanie obliczania wspolczynnika komfortu

--- app.py
+++ app.py
@@ -11,9 +11,64 @@ from subprocess import check_output
 from flask_socketio import SocketIO, send, emit
 import threading
 from subscribe_mqtt import MqttServer, mqtt
+import math
+import numpy as np
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+def PMV(Temp):
+
+    Ti = Temp
+    M = 100.00                                  # poziom metabolizmu [W/m3]
+    W = 10.0                                    # praca zewnetrzna [W/m3]
+    Pa = 1800.0                                 # czastkowe cisnienie pary wodnej [Pa]
+    Tr = 20.0                                   # sr. temperatura promieniowania
+    Tcl = 15.0                                  # temp. powierzchni odziezy
+    Fcl = 0.7                                   # stosunek pola powierzchni ciala odkrytego do ciala zakrytego odzieza
+    Hcl = 7                                     # wsplczynnik przejmowania ciepla prze konwekcje [W/m2]
+    PMV = 0
+    # Icl = 0.23                                # opor cieplny odziezy
+
+    a1 = 0.303 * math.exp(-0.036 * M + 0.028)
+    a2 = (M - W)
+    # Tcl= 35.7 -0.028*a2 -Icl*(3.96*pow(10,-8)*Fcl)*(((Tcl+273)*4-(Tr+274)*4))+Fcl*Hcl*(Tcl-Ti)
+    a3 = 3.05 * pow(10, -3) * (5733 - 6.99 * (M - W) - Pa)
+    a4 = 0.42 * ((M - W) - 58.15)
+    a5 = 1.7 * pow(10, -5) * M * (5867 - Pa)
+    a6 = 0.001 * 4 * M * (34 - Ti)
+    a7 = 3.96 * pow(10, -8) * Fcl * ((Tcl + 273) * 4 - (Tr + 274) * 4)
+    a8 = Fcl * Hcl * (Tcl - Ti)
+    PMV = (a1) * (a2 - a3 - a4 - a5 - a6 - a7 - a8)
+    return PMV
+
+def PMV_RGB(PMV):
+
+    # PMV has value from -3 to 3 - create list
+    PMVrange = np.arange(-3, 3, 0.1)
+
+    # match closest PMV value tovalue from created list
+    index = closest(PMVrange, PMV)
+
+    #generate list of color from red to green with lenght the same as PMVrange
+    #red = Color("red")
+    #colors = list(red.range_to(Color("green"), len(PMVrange)))
+
+    # or gradient from blue via green to red
+    col=list(["#0054E5", "#0061E4", "#006EE3", "#007BE3", "#0088E2", "#0095E1", "#00A2E1", "#00AFE0", "#00BCE0", "#00C8DF", "#00D5DE", "#00DEDA", "#00DDCD", "#00DDBF", "#00DCB2", "#00DBA4", "#00DB97", "#00DA89", "#00DA7C", "#00D96F", "#00D862", "#00D855", "#00D748", "#00D73B", "#00D62E", "#00D521", "#00D515", "#00D408", "#03D400", "#10D300", "#1CD200", "#29D200", "#35D100","#41D100", "#4DD000", "#59CF00", "#65CF00", "#71CE00", "#7DCE00", "#89CD00", "#94CC00", "#A0CC00","#ACCB00", "#B7CB00", "#C2CA00", "#C9C500", "#C9B800", "#C8AC00", "#C8A000", "#C79400", "#C68700", "#C67B00", "#C56F00", "#C56300", "#C45700", "#C34C00","#C34000", "#C23400", "#C22800", "#C11D00", "#C01100", "#C00600", "#BF0004", "#BF000F"])
+
+    # len(PMVrange) = len(colorse) -> so we can take the same value from color list
+    PMVcolor = col[index]
+
+    #return background color in hex
+    return PMVcolor
+
+def closest(list, Number):
+    aux = []
+    for valor in list:
+        aux.append(abs(Number-valor))
+
+    return aux.index(min(aux))
 
 class CMD:
     GET_WIFI_LIST = 0x01
@@ -128,7 +183,8 @@ def home():
         #Pobranie wszystkich sensorów z bazy danych oraz najnowszych danych
         for sensor in db_sensors:
             db_last_measured = db.last_record(sensor)
-            sensor_list.append([sensor, db_last_measured[0], db_last_measured[1], db_last_measured[2] ])
+            temp = float(db_last_measured[0])
+            sensor_list.append([sensor, db_last_measured[0], db_last_measured[1], db_last_measured[2], round(PMV(temp),3), PMV_RGB(PMV(temp))])
         print(sensor_list)
 
         return render_template('index.html', list = sensor_list)
